862792c4b7c0656f43b2af87dce4c89148c7dacf
Rosangel <miranda1401@hotmail.com>
2019-12-10 18:58:20 -0400

    web

---
+++ CLUBPROGFINALA/CLUBPROGFINALA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CLUBPROGFINALA.Startup))]
+namespace CLUBPROGFINALA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
